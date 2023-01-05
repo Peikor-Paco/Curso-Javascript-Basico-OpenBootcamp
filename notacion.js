9c2c881c3f42fff5843e7e08b59c55d4cc833867
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-05 12:42:31 +0100

    Notación en JavaScript

---
+++ notacion.js
@@ -0,0 +1,32 @@
+// Notación en JavaScript
+
+// ; --> delimitar el final de una linea
+let b = 2;
+console.log(b);
+
+
+
+
+// [] -> listas, arrays 
+// 1. para difinirlos
+const ar = [1,2,3,4]
+// 2. para acceder a un posiucion del array
+console.log(ar[2]);
+console.log(ar[3]);
+
+
+// () --> Se utiliza en Funciones
+function suma (a,b) {
+
+}
+
+// {} --> Se utiliza para objetos, funciones y estructuras de control
+
+const movil = {
+    ancho: 5,
+    altura: 22,
+}
+
+// . --> Se utiliza en los objetos para acceder a los atributos
+movil.ancho;
+console.log(movil.ancho);
