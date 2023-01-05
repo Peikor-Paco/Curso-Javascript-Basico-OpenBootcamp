932bed7bd301aae5204b14e7ee929b1f1e7e7b00
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-05 12:13:29 +0100

    tipos primitivos

---
+++ tipos-primitivos.js
@@ -0,0 +1,33 @@
+//Tipos Primitivos
+
+//number
+
+4;
+0;
+
+// string
+
+"Hola Mundo";
+'Hola Mundo';
+`Hola Mundo`;
+
+// booleanos
+
+true;
+false;
+
+
+// nulo y undefined
+
+null;
+undefined;
+
+// null, undefined, false, 0 = todas son falsas
+
+if(false) {
+    //si se cumple
+    console.log("cumple")
+}else {
+    //no se cumple
+    console.log("no se cumple")
+}
