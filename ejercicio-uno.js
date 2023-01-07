34b44cd97c3ae9d3fa19778a29872a660216394f
morenolopezfr@gmail.com <morenolopezfr@gmail.com>
2023-01-07 20:21:30 +0100

    Ejercicio Uno

---
+++ ejercicio-uno.js
@@ -0,0 +1,17 @@
+
+
+let nombre = "Francisco Moreno";
+let edad = 46;
+let miCumpleano = new Date(1976,08,27);
+let isDesarrollador = false;
+
+const book = {
+    titulo: "Las puertas de la percepcion",
+    autor: "Aldous Huxley",
+    fecha: 1954,
+    url: "https://es.wikipedia.org/wiki/Las_puertas_de_la_percepci%C3%B3n",
+}
+
+const sobreMi = [nombre, edad, isDesarrollador, miCumpleano, book];
+
+console.log(sobreMi);
